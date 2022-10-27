c6ec22909b7a2f3c9c83d2689113de7622d4b38c
deejoseph281 <adeeba23@gmail.com>
2022-10-27 17:44:42 -0500

    test

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -2,7 +2,7 @@
 import csv
 import os
 #Assign a variable to load a file from a path.
-file_to_load = 'election_results.csv'
+file_to_open = os.path.join("..", "Resources", "election_results.csv")
 #Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -17,7 +17,7 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 #Open the election results and read the file.
-with open(file_to_load) as election_data:
+with open(file_to_open) as election_data:
     file_reader = csv.reader(election_data)
 #Print the header row.
     headers = next(file_reader)
